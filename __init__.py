72181d4d71def305fdc27a4a0a6a56235e6cf463
Rupert Wu <wu.rupert@outlook.com>
2021-06-17 19:51:58 -0400

    Better filenames.

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
-from . import space_0, space_1A, space_1B, space_1C, space_1D
+from . import types_0, types_1A, types_1B, types_1C, types_1D
 
 spaces = {
-    "0": space_0.functions,
-    "1A": space_1A.functions,
-    "1B": space_1B.functions,
-    "1C": space_1C.functions,
-    "1D": space_1D.functions,
+    "types_0": types_0.functions,
+    "types_1A": types_1A.functions,
+    "types_1B": types_1B.functions,
+    "types_1C": types_1C.functions,
+    "types_1D": types_1D.functions,
 }
 
 ns_branches = {
-    "0": space_0.n_branches,
-    "1A": space_1A.n_branches,
-    "1B": space_1B.n_branches,
-    "1C": space_1C.n_branches,
-    "1D": space_1D.n_branches,
+    "types_0": types_0.n_branches,
+    "types_1A": types_1A.n_branches,
+    "types_1B": types_1B.n_branches,
+    "types_1C": types_1C.n_branches,
+    "types_1D": types_1D.n_branches,
 }
