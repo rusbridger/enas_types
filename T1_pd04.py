e26ac625b689e6607a5f1e700f0155f6ea514648
Rupert Wu <wu.rupert@outlook.com>
2021-08-17 20:04:55 -0400

    Balanced new convs with pooling at 2:1 ratio.

--- T1_pd04.py
+++ T1_pd04.py
@@ -1,11 +1,14 @@
-from .generate_convs import ConvSettings
-
 from lib.model.conv_branch import ConvBranch
 from lib.model.pool_branch import PoolBranch
 
+from .generate_convs import ConvSettings
+
+from math import ceil
+
 settings = ConvSettings(0, 0, 3, 1, 1,
                         1).generate_settings_type_eq(None, (1, 4), (1, 4))
-n_branches = 6 + len(settings)
+extra_pools = int(ceil(len(settings) / 4))
+n_branches = 6 + len(settings) + 2 * extra_pools
 
 
 def set_func(layer, in_planes, out_planes):
@@ -25,12 +28,18 @@ def set_func(layer, in_planes, out_planes):
     layer.branch_4 = PoolBranch(in_planes, out_planes, 'avg')
     layer.branch_5 = PoolBranch(in_planes, out_planes, 'max')
 
-    for i in range(len(settings)):
+    for (i, setting) in enumerate(settings):
         setattr(
             layer, "branch_{}".format(6 + i),
-            ConvBranch(in_planes, out_planes, settings[i].kernel_size,
-                       settings[i].padding, settings[i].dilation,
-                       settings[i].stride))
+            ConvBranch(in_planes, out_planes, setting.kernel_size,
+                       setting.padding, setting.dilation, setting.stride))
+
+    for i in range(6 + len(settings), 6 + len(settings) + extra_pools):
+        max_pool_i = i + extra_pools
+        setattr(layer, "branch_{}".format(i),
+                PoolBranch(in_planes, out_planes, 'avg'))
+        setattr(layer, "branch_{}".format(max_pool_i),
+                PoolBranch(in_planes, out_planes, 'max'))
 
     return n_branches
 
